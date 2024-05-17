11b7c49549065637356521054edcc5e42dcdefd4
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-05-17 20:28:59 +0530

    Update banall.py

--- SHUKLAMUSIC/plugins/admins/banall.py
+++ SHUKLAMUSIC/plugins/admins/banall.py
@@ -2,7 +2,7 @@ from SHUKLAMUSIC import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from SHUKLAMUSIC.utils.vip_ban import admin_filter
+from SHUKLAMUSIC.utils.shukla_ban import admin_filter
 from SHUKLAMUSIC.misc import SUDOERS
 
 BOT_ID = app.me.id  # Corrected this line

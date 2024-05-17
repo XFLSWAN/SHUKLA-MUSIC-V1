b860e84322f1c732b91d1b7900d69110ba2bc9f6
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-05-17 19:18:33 +0530

    Add files via upload

---
+++ SHUKLAMUSIC/plugins/misc/autobroadcast.py
@@ -0,0 +1,112 @@
+import asyncio
+import datetime
+from SHUKLAMUSIC import app
+from pyrogram import Client
+from SHUKLAMUSIC.utils.database import get_served_chats
+from config import START_IMG_URL, AUTO_GCAST_MSG, AUTO_GCAST, LOGGER_ID
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+AUTO_GCASTS = f"{AUTO_GCAST}" if AUTO_GCAST else False
+
+START_IMG_URLS = "https://graph.org/file/760169f7f8dd536c50793.jpg"
+
+MESSAGES = f"""**✤ ɪɴᴛʀᴏᴅᴜᴄɪɴɢ ᴏᴜʀ ➻ ᴛʜᴇ ᴍᴏsᴛ ᴩᴏᴡᴇʀғᴜʟ ᴛᴇʟᴇɢʀᴀᴍ ✧ ɢʀᴏᴜᴩ ᴍᴀɴᴀɢᴇᴍᴇɴᴛ ʀᴏʙᴏᴛs ✧ ᴡɪᴛʜ sᴏᴍᴇ ᴀᴡᴇsᴏᴍᴇ ᴀɴᴅ ᴜsᴇғᴜʟ ғᴇᴀᴛᴜʀᴇs ᴛʜᴀᴛ ᴡᴀs ʙᴜɪʟᴛ ᴛᴏ ʜᴇʟᴘ ʏᴏᴜ ᴍᴀɴᴀɢᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴇᴀꜱɪʟʏ ᴀɴᴅ ᴛᴏ ᴘʀᴏᴛᴇᴄᴛ ʏᴏᴜʀ ɢʀᴏᴜᴘ ꜰʀᴏᴍ ꜱᴄᴀᴍᴍᴇʀꜱ ᴀɴᴅ ꜱᴘᴀᴍᴍᴇʀꜱ🌹
+
+┏━━━━━━✦
+┣✧  ❖    [𝗦ᴇɴ‌ᴏʀɪᴛᴀ ✘ 𝗥ᴏʙᴏᴛ](https://t.me/StrangerSuperbot)
+┣✧  ❖    [𝗠ᴇʟᴀɴɪᴀ ✘ 𝗥ᴏʙᴏᴛ](https://t.me/Melaniarobot)
+┗━━━━━━✦
+
+**"""
+
+
+BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                "๏ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ๏",
+                url=f"https://t.me/StrangerSuperbot?startgroup=true",
+            )
+        ]
+        [
+            InlineKeyboardButton(
+                "๏ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ๏",
+                url=f"https://t.me/Melaniarobot?startgroup=true",
+            )
+        ]
+    ]
+)
+
+MESSAGE = f"""**๏ ᴛʜɪs ɪs ᴀᴅᴠᴀɴᴄᴇᴅ ᴍᴜsɪᴄ ᴘʟᴀʏᴇʀ ʙᴏᴛ ғᴏʀ ᴛᴇʟᴇɢʀᴀᴍ ɢʀᴏᴜᴘs + ᴄʜᴀɴɴᴇʟs ᴠᴄ. 💌
+
+🎧 ᴘʟᴀʏ + ᴠᴘʟᴀʏ + ᴄᴘʟᴀʏ 🎧
+
+➥ sᴜᴘᴘᴏʀᴛᴇᴅ ᴡᴇʟᴄᴏᴍᴇ - ʟᴇғᴛ ɴᴏᴛɪᴄᴇ, ᴛᴀɢᴀʟʟ, ᴠᴄᴛᴀɢ, ʙᴀɴ - ᴍᴜᴛᴇ, sʜᴀʏʀɪ, ʟᴜʀɪᴄs, sᴏɴɢ - ᴠɪᴅᴇᴏ ᴅᴏᴡɴʟᴏᴀᴅ, ᴇᴛᴄ... ❤️
+
+🔐ᴜꜱᴇ » [/start](https://t.me/{app.username}?start=help) ᴛᴏ ᴄʜᴇᴄᴋ ʙᴏᴛ
+
+➲ ʙᴏᴛ :** @{app.username}"""
+
+BUTTON = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                "๏ ᴋɪᴅɴᴀᴘ ᴍᴇ ๏",
+                url=f"https://t.me/Itz_SapnaMusicbot?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users",
+            )
+        ]
+    ]
+)
+
+caption = f"""{AUTO_GCAST_MSG}""" if AUTO_GCAST_MSG else MESSAGES
+
+TEXT = """**ᴀᴜᴛᴏ ɢᴄᴀsᴛ ɪs ᴇɴᴀʙʟᴇᴅ sᴏ ᴀᴜᴛᴏ ɢᴄᴀsᴛ/ʙʀᴏᴀᴅᴄᴀsᴛ ɪs ᴅᴏɪɴ ɪɴ ᴀʟʟ ᴄʜᴀᴛs ᴄᴏɴᴛɪɴᴜᴏᴜsʟʏ. **\n**ɪᴛ ᴄᴀɴ ʙᴇ sᴛᴏᴘᴘᴇᴅ ʙʏ ᴘᴜᴛ ᴠᴀʀɪᴀʙʟᴇ [ᴀᴜᴛᴏ_ɢᴄᴀsᴛ = (ᴋᴇᴇᴘ ʙʟᴀɴᴋ & ᴅᴏɴᴛ ᴡʀɪᴛᴇ ᴀɴʏᴛʜɪɴɢ)]**"""
+
+
+async def send_text_once():
+    try:
+        await app.send_message(LOGGER_ID, TEXT)
+    except Exception as e:
+        pass
+
+
+async def send_message_to_chats():
+    try:
+        chats = await get_served_chats()
+
+        for chat_info in chats:
+            chat_id = chat_info.get("chat_id")
+            if isinstance(chat_id, int):  # Check if chat_id is an integer
+                try:
+                    await app.send_photo(
+                        chat_id,
+                        photo=START_IMG_URLS,
+                        caption=caption,
+                        reply_markup=BUTTONS,
+                    )
+                    await asyncio.sleep(
+                        20
+                    )  # Sleep for 100 second between sending messages
+                except Exception as e:
+                    pass  # Do nothing if an error occurs while sending message
+    except Exception as e:
+        pass  # Do nothing if an error occurs while fetching served chats
+
+
+async def continuous_broadcast():
+    await send_text_once()  # Send TEXT once when bot starts
+
+    while True:
+        if AUTO_GCAST:
+            try:
+                await send_message_to_chats()
+            except Exception as e:
+                pass
+
+        # Wait for 100000 seconds before next broadcast
+        await asyncio.sleep(100000)
+
+
+# Start the continuous broadcast loop if AUTO_GCAST is True
+if AUTO_GCAST:
+    asyncio.create_task(continuous_broadcast())

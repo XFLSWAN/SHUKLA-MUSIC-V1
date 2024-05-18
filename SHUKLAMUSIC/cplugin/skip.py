253fd98b23400013fd71fd66fe023611afa28858
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-05-18 05:35:50 +0530

    Update skip.py

--- SHUKLAMUSIC/cplugin/skip.py
+++ SHUKLAMUSIC/cplugin/skip.py
@@ -52,7 +52,7 @@ async def skip(cli, message: Message, _, chat_id):
                                         ),
                                         reply_markup=close_markup(_),
                                     )
-                                    await VIP.stop_stream(chat_id)
+                                    await SHUKLA.stop_stream(chat_id)
                                 except:
                                     return
                                 break

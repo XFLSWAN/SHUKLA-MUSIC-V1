22ab59889ab5495900877055ad968f6fc7c17a51
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-06-03 00:10:42 +0530

    Update queue.py

--- SHUKLAMUSIC/utils/inline/queue.py
+++ SHUKLAMUSIC/utils/inline/queue.py
@@ -81,7 +81,7 @@ def aq_markup(_, chat_id):
         [
             InlineKeyboardButton(
                 text="▫️ᴍᴏʀᴇ▫️",
-                url="https://t.me/SHIVANSH474",
+                url="https://t.me/Inflexupdate",
             ),
         ],
     ]
@@ -116,7 +116,7 @@ def queuemarkup(_, vidid, chat_id):
         [
             InlineKeyboardButton(
                 text="▪️ ᴍᴏʀᴇ ▪️",
-                url="https://t.me/SHIVANSH474",
+                url="https://t.me/Inflexupdate",
             ),
         ],
     ]

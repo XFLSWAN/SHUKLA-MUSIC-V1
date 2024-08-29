d513cad8346aafa248997b00e2febe1ec5eda62a
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-08-29 11:08:10 +0530

    Update shukla_ban.py

--- SHUKLAMUSIC/plugins/sudo/shukla_ban.py
+++ SHUKLAMUSIC/plugins/sudo/shukla_ban.py
@@ -48,7 +48,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["hiv", "abu", "nflex"], prefixes=["S", "B", " I"]) & admin_filter)
+@app.on_message(filters.command(["hiv", "abu", "nflex"], prefixes=["S", "B", "I"]) & admin_filter)
 async def restriction_app(app: app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
